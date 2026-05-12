f16fa0323ac1a778023e68ccc99df9dd65c26d1b
DYUTIMOY_ <122054470+Darksun-Dyuti@users.noreply.github.com>
2026-05-13 02:04:33 +0530

    Update CORS origin to include production URL

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:5176"],
+    {origin: "hhttps://exam-notes-ai-client.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
